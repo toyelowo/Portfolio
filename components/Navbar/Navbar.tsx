447f939c8b971670ced95f2d208860c24776132b
toyelowo <temitayo.oyelowo@shopify.com>
2022-08-07 19:13:06 -0400

    Fix navbar to use next/link

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,20 +1,30 @@
+import Link from 'next/link';
+
 import styles from '/styles/Navbar.module.scss';
 
-export function Navbar(){
-    
-    return (
-        <nav className={styles.Nav}>
-        <h2 className={styles.Nav__title}>
-          <a href="#">
+export function Navbar() {
+  return (
+    <nav className={styles.Nav}>
+      <h2 className={styles.Nav__title}>
+        <Link href="/">
+          <a>
             <span className={styles['Nav__title--span']}>Temitayo </span>
             <span className={styles['Nav__title--span']}>Oyelowo</span>
           </a>
-        </h2>
-          <ul className={styles.Nav__List}>
-            <li className={styles.Nav__List__item}><a href="#">About</a></li>
-            <li className={styles.Nav__List__item}><a href="#">Work</a></li>
-            <li className={styles.Nav__List__item}><a href="#">Contact</a></li>
-          </ul>
-          </nav>
-    )
+        </Link>
+      </h2>
+
+      <ul className={styles.Nav__List}>
+        <li className={styles.Nav__List__item}>
+          <Link href="/about"><a>About</a></Link>
+        </li>
+        <li className={styles.Nav__List__item}>
+          <Link href="/work"><a>Work</a></Link>
+          </li>
+        <li className={styles.Nav__List__item}>
+          <Link href="/contact"><a>Contact</a></Link>
+        </li>
+      </ul>
+    </nav>
+  )
 }
